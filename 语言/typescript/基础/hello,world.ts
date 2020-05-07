da8a184ed2e66db5ba6205ef084f405fd4fcc59e
zhaolinjie <zhaolinjie@qq.com>
2020-05-07 19:16:41 +0800

    updata

--- 语言/typescript/基础/hello,world.ts
+++ 语言/typescript/基础/hello,world.ts
@@ -35,7 +35,7 @@ const person: IPerson = {
 type IPerson2 = {
     name: string
     age: number
-    family: string[]
+    readonly family: string[] // readonly 只读属性
     sex?: '男' | '女'
 }
 
@@ -77,6 +77,93 @@ interface B { b: string }
 const a: A = { a: 1 };
 const b: B = { b: '1' };
 const ab: A & B = { ...{ a: 1 }, ...b };
+
+
+// 泛式方法
+function gen_func1<T>(arg: T): T {
+    return arg;
+}
+
+// 或者
+
+let gen_func2: <T>(arg: T) => T = function (arg) {
+    return arg;
+}
+
+// 泛型参数名可以随意命名
+function identity<U>(arg: U): U {
+    return arg;
+}
+
+// 定义泛型接口
+// 两种写法 第一种
+interface GenericIdentityFn {
+    <T>(arg: T): T;
+}
+
+// 第二种
+interface GenericIdentityFn2<T> {
+    (arg: T): T;
+}
+
+function identity1<T>(arg: T): T {
+    return arg;
+}
+
+let myIdentity: GenericIdentityFn2<number> = identity1;
+
+// 除了泛型接口，我们还可以创建泛型类。 注意，无法创建泛型枚举和泛型命名空间
+// 类有两部分：静态部分和实例部分。 泛型类指的是实例部分的类型，所以类的静态属性不能使用这个泛型类型。
+// 与接口一样，直接把泛型类型放在类后面，可以帮助我们确认类的所有属性都在使用相同的类型
+class GenericNumber<T> {
+    zeroValue: T;
+    add: (x: T, y?: T) => T;
+}
+
+// 创建一个实例里面的定义的泛型为number
+let myGenericNumber = new GenericNumber<number>();
+
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) { return x + y; };
+
+// 创建一个实例里面的定义的泛型为string
+let myGenericString = new GenericNumber<string>();
+
+
+// 泛型约束
+
+interface Lengthwise {
+    length: number;
+}
+
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+    console.log(arg.length, arg);
+
+    return arg;
+}
+
+// loggingIdentity(3) // Error, number doesn't have a .length property
+
+// 我们需要传入符合约束类型的值，必须包含必须的属性：
+loggingIdentity({ length: 10, value: 3 });
+
+
+// 类
+
+class momo {
+    // 当成员被标记成 private时，它就不能在声明它的类的外部访问
+    private name: string;
+
+    // 声明可保护变量 可在该类中 或者 派生类中访问
+    protected age: number;
+
+    // 公共属性 
+    public sex?: '男' | '女'; 
+
+    // 静态属性 
+    static momo: any;
+}
+
 
 
 
