07c7a4bbddef19bcc9052e3ce34ceea607e71571
CSC <634725355@qq.com>
2020-10-06 12:08:26 +0800

    悬浮球

--- 奇奇怪怪的代码/aesEncryption/index.js
+++ 奇奇怪怪的代码/aesEncryption/index.js
@@ -1,5 +1,5 @@
 const cryptoJS = require('./aes_ecb/test');
 
-console.log(cryptoJS.Decrypt("65ZKxC8byB3jqUIaBcigPHuqJ6VWuBvPlw6aysxjxtsCvaC25CIy9zYk4IJTSr86"));
+console.log(cryptoJS.Decrypt("yp3ye9cuklb/W94IWThBemwiqGfuT9t2xa4gC+HNfdCmoCc0OMOJOEhXrt5iPDWqT1ajpyMJubedhEf0BYRlw0QqR1XAl2SAZLaInQJp2rjIa2ropbiaN1Ww/s4ffuRv/tNiUwtzdZ7uSzYZIHb0iYOOf9cskWLzmGlPkBCaPRjt+mPEMNG/HiuZZRTPQpDaTVEUdofj3KvguP95ZrstNQ=="));
 
 console.log(cryptoJS.Encrypt(JSON.stringify({ momo: "哈哈哈哈哈" })));
