015ea968920ec7343a464b9760bced1a9a72e56f
互联网+-赵林杰 <634725355@qq.com>
2020-11-09 21:44:11 +0800

    构造函数

--- 语言/typescript/constructor.ts
+++ 语言/typescript/constructor.ts
@@ -1,13 +1,23 @@
 {
+  
   class Person {
-    public name: string;
-    constructor(name: string) {
+    // 相当于定义了一个name函数
+    constructor(public name: string) {
       this.name = name;
     }
   }
 
+  class Teacher extends Person {
+    constructor(public age: number) {
+      super('kaka')
+    }
+  }
+
   const person = new Person("momo");
+  const teacher = new Teacher(18);
+
   console.log(person.name);
+  console.log(teacher.age, teacher.name);
   
 
 }
