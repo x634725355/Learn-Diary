1db67efcd0e89e19db92d50eb6760787c776a69d
互联网+-赵林杰 <634725355@qq.com>
2020-11-09 21:36:45 +0800

    构造函数

--- 语言/typescript/class.ts
+++ 语言/typescript/class.ts
@@ -14,12 +14,14 @@
 		sayLove() {
 			return "I love You";
 		}
+		sayHello() {
+			return super.sayHello()  + "Hi, honey!";
+		}
 	}
 
 	let goddess1 = new Sister();
 
 	console.log(goddess1.sayHello(), goddess1.sayLove());
-	
 
 }
 
---
+++ 语言/typescript/constructor.ts
@@ -0,0 +1,13 @@
+{
+  class Person {
+    public name: string;
+    constructor(name: string) {
+      this.name = name;
+    }
+  }
+
+  const person = new Person("momo");
+  console.log(person.name);
+  
+
+}
