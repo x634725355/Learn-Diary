1a0f49e38bf6e74af54f3bac18e4499947efb0c3
互联网+-赵林杰 <634725355@qq.com>
2020-11-10 09:56:43 +0800

    时间差异

--- 语言/typescript/constructor.ts
+++ 语言/typescript/constructor.ts
@@ -1,23 +1,24 @@
 {
-  
+
   class Person {
     // 相当于定义了一个name函数
-    constructor(public name: string) {
+    constructor(public name: string, public date: number) {
+      this.date = date;
       this.name = name;
     }
   }
 
   class Teacher extends Person {
     constructor(public age: number) {
-      super('kaka')
+      super('kaka', new Date().setMonth(24))
     }
   }
 
-  const person = new Person("momo");
+  const person = new Person("momo", new Date().setMonth(24));
   const teacher = new Teacher(18);
 
-  console.log(person.name);
+  console.log("人的任务", person.name, person.date);
   console.log(teacher.age, teacher.name);
-  
+
 
 }
