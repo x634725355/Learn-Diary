98aebc510052a333afb2893bae01eea5f9debbf2
互联网+-赵林杰 <634725355@qq.com>
2020-12-24 19:03:22 +0800

    b站首页头部动画

--- demo.js
+++ demo.js
@@ -4,3 +4,10 @@ let a = {a:1,b:2,c:3};
 for (const iterator of Object.keys(a)) {
     console.log(iterator);
 }
+
+
+let b = ['a','e','s','d','g','d','k','q','y','o','b','c'];
+
+let bDemo = b.sort();
+
+console.log("顺序", bDemo);
